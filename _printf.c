8fc1931ad8fcc7b7eaf9c111a6c73fc2a70cc5f3
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-08-11 16:12:16 +1000

    create the main function for printf project

---
+++ _printf.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format, similar to printf
+ * @format: character string containing directives to be formatted
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i;
+	int count;
+	va_list args;
+
+
+	va_start(args, format);
+	i = 0;
+	count = 0;
+	while (format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == 's')
+				{
+					count += print_string(args);
+					i++;
+				}
+				else if (format[i + 1] == '%')
+				{
+					count += print_percent(args);
+					i++;
+				}
+				else if (format[i + 1] == 'c')
+				{
+					count += print_char(args);
+					i++;
+				}
+				else if (format[i + 1] == '\0')
+				{
+					write(1, &format[i], 1);
+					count++;
+				}
+				else
+				{
+					write(1, &format[i], 1);
+					write(1, &format[i + 1], 1);
+					count += 2;
+					i++;
+
+				}
+			}
+			else
+			{
+				write(1, &format[i], 1);
+				count++;
+			}
+			i++;
+		}
+	va_end(args);
+	return (count);
+}
